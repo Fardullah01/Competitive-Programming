228df7eb957483c8d095d90b9dae4256211b4686
Fardullah01 <mdfardullah001@gmail.com>
2026-05-13 22:34:54 +0600

    new solution added

--- A. Koshary.cpp
+++ A. Koshary.cpp
@@ -1,1 +1,18 @@
+#include<bits/stdc++.h>
+#define endl '\n'
+#define fast ios::sync_with_stdio(0);cin.tie(0);
+using namespace std;
+int main()
+{
+    fast;
+    int t;
+    cin>>t;
+    while(t--)
+    {
+       int x,y;
+       cin>>x>>y;
+       if(x%2 && y%2)cout<<"NO\n";
+       else cout<<"YES\n";
+    }
+}
 
