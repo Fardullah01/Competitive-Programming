ee64b54ddfa79ae71c7750274f0f64f4399bc3b2
Fardullah01 <mdfardullah001@gmail.com>
2026-05-19 19:22:01 +0600

    solution added

--- B. Digit String.cpp
+++ B. Digit String.cpp
@@ -22,29 +22,36 @@ int main()
         if(c4 ==s.size())cout<<c4<<endl;
         else
         {
-       int x=0,y=v.size()-1;
-      for(int i=0;i<v.size();i++)
-      {
-          if(v[i]==1)
-          {  x=i;
-              break;}
-          else c0++;
-      }
-      for(int i=v.size()-1;i>=0;i--)
-      {
-          if(v[i]==0)
-          {  y=i;
-              break;}
-          else c1++;
-      }
-      for(int i=x;i<=y;i++)
-      {
-          if(v[i]==0)c00++;
-          else c11++;
-      }
-      cout<<min(c11,c00)+c4<<endl;
-    }
-}
+          //  for(auto it:v)cout<<it<<" ";
+          //  cout<<endl;
+            vector<int>v1,v0;
+            int x=0;
+            for(int i=0;i<v.size();i++)
+            {
+                if(v[i]==0)
+                {
+                    x++;
+                }
+                v0.push_back(x);
+            }
+            x=0;
+            for(int i=v.size()-1;i>=0;i--)
+            {
+                if(v[i]==1)
+                {
+                    x++;
+                }
+                v1.push_back(x);
+            }
+            int ans=-1;
+            reverse(v1.begin(),v1.end());
+            for(int i=0;i<v1.size();i++)
+            {
+                ans=max(ans,v1[i]+v0[i]);
+            }
+            cout<<c4+v1.size()-ans<<endl;
+        }
+  }
 }
 
 
